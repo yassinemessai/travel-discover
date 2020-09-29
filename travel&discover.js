b95fc6586947f6c6cd84725a160489e400c40c12
yassinemessai <70016132+yassinemessai@users.noreply.github.com>
2020-09-29 12:07:57 +0100

    solo project

---
+++ travel&discover.js
@@ -0,0 +1,14 @@
+var de=["https://drive.google.com/drive/folders/1S-Sx0Hdtazk-YqfddeD1mR-0_9UnaY40?usp=sharing"]
+var fr=["https://drive.google.com/drive/folders/11ys-IzrXlmirRLc6wFGIVgt-1Z83spVV?usp=sharing"]
+var nl=["https://drive.google.com/drive/folders/1jJO2tDTJGP4eTL-5OsRHVD2HwqnltPRf?usp=sharing"]
+var it=["https://drive.google.com/drive/folders/1tS2dI1MGBG2EHGnktYYssZdKe7yD1hEc?usp=sharing"]
+var pt=["https://drive.google.com/drive/folders/1nB3z6CqLBkXOEjkWKSYYS8c8SfWJwh2p?usp=sharing"]
+var es=["https://drive.google.com/drive/folders/1tSeG15MqUrghPxUMmyX4UvWCf7lKQvrZ?usp=sharing"]
+var uk=["https://drive.google.com/drive/folders/1iN0BH0jplMCvcLgyuqhm7EqiZ5EFBAuD?usp=sharing"]
+
+
+var mr=["https://drive.google.com/drive/folders/13-pwj0-mRZRiO7sjl-qS-hNcSTkgAwsP?usp=sharing"]
+var sn=["https://drive.google.com/drive/folders/1JxzaRLz0B4ddVnYdqCtjRCodUSTUZGJz?usp=sharing"]
+var cv=["https://drive.google.com/drive/folders/13ZdQ0DOeUIhVu5K4qTjxAcFVD5nHR9Er?usp=sharing"]
+var ci=["https://drive.google.com/drive/folders/1EgkM2ABicip8xtMpSr4u-Dyulap2thPQ?usp=sharing"]
+var lasp=["https://drive.google.com/drive/folders/1u5tyeXc6tNtxoOgUFw9Vn-XpZeeejIx9?usp=sharing"]
